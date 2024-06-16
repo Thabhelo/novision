994f6712f9d7e3016768744d1369a2c1215c4991
Thabhelo <thabhelo.duve@talladega.edu>
2024-06-16 14:30:22 -0400

    Initial Commit

---
+++ novision/novisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  novisionApp.swift
+//  novision
+//
+//  Created by Thabhelo Duve on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct novisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
