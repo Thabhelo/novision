e9d888add3a56fd4a5f4061e96e607c318d46a3c
Thabhelo <thabhelo.duve@talladega.edu>
2024-07-15 12:15:27 -0400

    fixed directory errors

--- dashboard/venv/bin/activate.fish
+++ dashboard/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/thabheloduve/Desktop/health-monitoring-dashboard/venv"
+set -gx VIRTUAL_ENV "/Users/thabheloduve/Desktop/novision/dashboard/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
